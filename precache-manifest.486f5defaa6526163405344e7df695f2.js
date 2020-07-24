123f46d38d0e25144e3e87ed0ad36ad7ed4ba93f
Ivan Nosar <wowgm@yandex.ru>
2020-07-25 00:50:12 +0300

    Updates

---
+++ precache-manifest.486f5defaa6526163405344e7df695f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0dd5ecd1853665075d45e9d73f67efa",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2ddd95dfe4aba5c2c4c6",
+    "url": "./static/css/main.bcfb8f81.chunk.css"
+  },
+  {
+    "revision": "2181d7fef9a64da0fa6c",
+    "url": "./static/js/2.420fa007.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.420fa007.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ddd95dfe4aba5c2c4c6",
+    "url": "./static/js/main.ff484671.chunk.js"
+  },
+  {
+    "revision": "d98948e661c564b68248",
+    "url": "./static/js/runtime-main.ab644a26.js"
+  }
+]);
